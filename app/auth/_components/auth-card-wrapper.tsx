c2799cac82137705c016bcd7feb2c152fbe915c2
sajawalhassan <sajawalhassan.1feb@gmail.com>
2024-07-11 13:37:00 +0500

    more vercel shit

--- app/auth/_components/auth-card-wrapper.tsx
+++ app/auth/_components/auth-card-wrapper.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ChevronLeftIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
 type Props = {
   heading: string;
@@ -15,22 +15,24 @@ export const AuthCardWrapper = (props: Props) => {
   const router = useRouter();
 
   return (
-    <div className="flex min-h-screen w-screen items-center justify-center px-2">
-      <div className="relative w-full max-w-[25rem] rounded-md bg-black/20 p-4 text-white shadow-md shadow-black">
-        <TooltipProvider>
-          <Button
-            size={"icon"}
-            onClick={() => router.back()}
-            className="absolute left-4 top-4"
-          >
-            <ChevronLeftIcon />
-          </Button>
-          <h1 className="mb-4 text-center text-3xl font-bold">
-            {props.heading}
-          </h1>
-          {props.children}
-        </TooltipProvider>
+    <Suspense>
+      <div className="flex min-h-screen w-screen items-center justify-center px-2">
+        <div className="relative w-full max-w-[25rem] rounded-md bg-black/20 p-4 text-white shadow-md shadow-black">
+          <TooltipProvider>
+            <Button
+              size={"icon"}
+              onClick={() => router.back()}
+              className="absolute left-4 top-4"
+            >
+              <ChevronLeftIcon />
+            </Button>
+            <h1 className="mb-4 text-center text-3xl font-bold">
+              {props.heading}
+            </h1>
+            {props.children}
+          </TooltipProvider>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
