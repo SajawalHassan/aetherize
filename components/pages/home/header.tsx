5e9b0cac6ed19ca05a424104c396f03b381d3d2b
sajawalhassan <sajawalhassan.1feb@gmail.com>
2025-12-23 13:37:38 +0500

    Create header (mobile)

--- components/pages/home/header.tsx
+++ components/pages/home/header.tsx
@@ -1,9 +1,16 @@
+import MenuIcon from "@/assets/menu.svg";
+import Image from "next/image";
+
 type Props = {};
 
 export const Header = (props: Props) => {
   return (
     <div className="pt-2 px-4 flex items-center justify-between">
       <h1 className="font-finlandica text-[35px] font-bold">Aetherize</h1>
+
+      <button>
+        <Image src={MenuIcon} alt="Menu Icon" width={38} height={38} />
+      </button>
     </div>
   );
 };
